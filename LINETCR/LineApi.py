101e45a942416b9b6dc141c79fd9cf197aee029c
Redsamuri <34795680+Redsamuri@users.noreply.github.com>
2018-03-06 17:56:18 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -424,9 +424,9 @@ class LINE:
 
       prof = self.getProfile()
 
-      print("===============[Chucky_Bot]================")
+      print("===============[Redsamuri_Bot]================")
       print("        Thanks for TCR and my friend")
-      print("===============[© By_Nadya]================")
+      print("===============[© By_Red]================")
       print("mid -> " + prof.mid)
       print("name -> " + prof.displayName)
       print("authToken -> " + self.authToken)
